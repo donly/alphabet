c65c7c43a9d5b224dda6de4b68ab22604b8db839
Dong <donly@magicalboy.com>
2020-08-19 19:50:57 +0800

    first version

--- alphabet/ContentView.swift
+++ alphabet/ContentView.swift
@@ -9,8 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var alphabet = "A"
+  private let alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  @State private var isPronouce = false
+  private let colors: [Color] = [.orange, .red, .yellow, .green, .blue, .purple, .white, .gray]
+  private let truefalse = [true, false]
     var body: some View {
-        Text("Hello, World!")
+      ZStack {
+        colors.randomElement()!
+          .opacity(0.4)
+          .edgesIgnoringSafeArea(.all)
+        Circle().strokeBorder(colors.randomElement()!, lineWidth: 30)
+          .aspectRatio(contentMode: .fit)
+          .overlay(Text(truefalse.randomElement()! ? alphabet : alphabet.lowercased()).foregroundColor(colors.randomElement()!).font(.system(size: 500)).minimumScaleFactor(0.01)
+            .shadow(radius: 4)
+            .padding()
+        )
+          .edgesIgnoringSafeArea(.all)
+          .padding()
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+//          .background(colors.randomElement()!)
+          .onTapGesture {
+            if !self.isPronouce {
+              let pronouncer = WordPronouncer()
+              pronouncer.pronounce(self.alphabet.lowercased())
+              self.isPronouce = true
+            }
+            else {
+              self.alphabet = self.alphabets.randomElement()!
+              self.isPronouce = false
+            }
+        }
+      }
     }
 }
 
